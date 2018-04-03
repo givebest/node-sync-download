86c45c01bdb6d889ff22325dde75d68cd22e1efc
givebest <givenlovs@msn.com>
2018-04-03 15:36:03 +0800

    + init   完成 README.md 书写   完成 app.js 演示程序

---
+++ README.md
@@ -0,0 +1,116 @@
+# Node.js sync download
+---
+
+## 介绍
+
+在 Node.js 8.1 的基础上，通过 `Await` 、`Async`、`Promise` 实现的同步顺序执行，顺序下载图片的程序。
+
+###  了解 JavaScript 中同步执行（ Await、Async、Promise）
+
+##### Await
+
+Await 会暂停当前 `async function` 的执行，等待 Promise 处理完成。
+
+##### Async 
+
+Asyn 函数中若有 await 表达式，这会使 async 函数暂停执行，等待表达式中的 Promise 解析完成后继续执行 async 函数并返回 resolve  结果。
+
+##### Promise
+
+解决回调地狱问题，可以像写同步代码一样写异步操作。
+
+#### 组合实现同步执行
+
+```js
+'use strict';
+/**
+ * 第一个任务
+ */
+function task1 () {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('1', '我是第一个任务，必须第一个执行');
+      resolve('done');
+    }, 3000);
+  });
+}
+
+/**
+ * 第二个任务
+ */
+function task2 () {
+
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('2', '第二个任务');
+      resolve('done');
+    }, 1000)
+  });
+}
+
+/**
+ * 第三个任务
+ */
+function task3 () {
+  return new Promise((resolve, reject) => {
+    console.log('3', '第三个任务');
+    reject('error');
+  });
+}
+
+/**
+ * 第四个任务
+ */
+function task4 () {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('4', '第四个任务');
+      resolve('done');
+    }, 2000);
+  })
+}
+
+/**
+ * 所有任务
+ */
+async function allTasks () {
+  await task1();
+  await task2();
+  await task3();
+  await task4();
+}
+
+// 执行任务
+allTasks();
+
+/**
+ * 3秒后
+ * 1 我是第一个任务，必须第一个执行
+ * 1秒后
+ * 2 第二个任务
+ * 3 第三个任务
+ * Uncaught (in promise) error
+ */
+```
+
+## 使用
+
+> Node.js 推荐最新版或其它版本，但保证支持 `Await` 、`Async`
+
+### 安装 
+
+```shell
+npm install
+```
+
+### 运行
+
+```shell
+node app
+```
+
+## License
+
+MIT  © 2018 [givebest](https://github.com/givebest)
+
+
---
+++ app.js
@@ -0,0 +1,158 @@
+'use strict';
+
+const fs = require('fs');
+const fse = require('fs-extra');
+const path = require('path');
+const cheerio = require('cheerio');
+const request = require('request');
+
+const config = {
+  url: 'https://movie.douban.com/subject/26942631/photos?type=S' // 豆瓣电影图片列表
+};
+
+/**
+ * 获取 HTML
+ * @return {[type]}     [description]
+ */
+function getHtml (url) {
+  // @see https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Promise
+  return new Promise(resolve => {
+    // @see https://github.com/request/request
+    request(url, (error, response, body) => {
+      // 出错时不要使用 reject，会抛出异常，停止运行
+      // @see https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/await
+      if (error) {
+        console.log('error', error);
+        resolve('error');
+        return;
+      }
+      resolve(body);
+    });
+  });
+}
+
+/**
+ * 解析 HTML
+ * @return {[type]}      [description]
+ */
+function parseHtml (html) {
+  return new Promise(resolve => {
+    if (!html || html === 'error') {
+      resolve('error');
+      return;
+    }
+
+    // 解析出电影名字和图片集合
+    let $ = cheerio.load(html);
+    let name = $('#content > h1').text(),
+      items = $('ul.poster-col3 > li'),
+      images = [];
+
+    name = name.replace('的剧照', '');
+
+    items.each(function () {
+      let img = $(this).find('img').attr('src');
+      if (img) {
+        images.push(img);
+      }
+    });
+
+    resolve({
+      name:name,
+      images: images
+    });
+  });
+}
+
+/**
+ * 获取图片
+ * @return {[type]}     [description]
+ */
+// @see https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Statements/async_function
+async function getImage (opts) {
+  if (opts === 'error') {
+    return 'error'
+  }
+
+  opts = opts || {};
+  let movieName = opts.name,
+    images = opts.images || [];
+
+  if (images.length <= 0) {
+    return 'error';
+  }
+
+  // 根据电影名字创建目录
+  let dir = 'download/' + movieName + '/';
+  creatDir(dir);
+
+  // 遍历图片下载
+  for (var i = images.length - 1; i >= 0; i--) {
+    let img = images[i],
+      imgName = path.basename(img);
+    await saveImage({
+      url: img,
+      dir:dir + imgName
+    });
+  }
+}
+
+/**
+ * 保存图片
+ * @return {[type]}      [description]
+ */
+function saveImage (opts) {
+  opts = opts || {};
+  let url = opts.url,
+    dir = opts.dir;
+
+  return new Promise (resolve => {
+    if (!url || !dir) {
+      resolve('error');
+      return;
+    }
+
+    // 单个保存完成后继续下一个
+    setTimeout(() => {
+
+      // 检查图片是否已存在
+      if (fs.existsSync(dir)) {
+        console.log('Exist', dir);
+        resolve('exist');
+        return;
+      }
+
+      // 请求并保存图片
+      request(url, (error, response, body) => {
+        if (error) {
+          console.log('error', error);
+          resolve('error');
+          return;
+        }
+      }).pipe(fs.createWriteStream(dir));
+      console.log('Saved', dir);
+      resolve('done');
+    }, 1000);
+  });
+}
+
+/**
+ * 创建目录
+ * @return {[type]}     [description]
+ */
+function creatDir (dir) {
+  // @see https://github.com/jprichardson/node-fs-extra
+  fse.ensureDirSync(dir)
+}
+
+/**
+ * 初始化
+ * @return {[type]} [description]
+ */
+async function init () {
+  let html = await getHtml(config.url);
+  let opts = await parseHtml(html);
+  await getImage(opts);
+}
+
+init()
